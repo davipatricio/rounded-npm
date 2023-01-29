8ef8c3f547506924fff213ad48d991ce77d42abf
Davi Patricio <davipatricio2006@gmail.com>
2023-01-29 14:20:26 +0000

    add page title

---
+++ src/app/[packageName]/head.tsx
@@ -0,0 +1,13 @@
+interface PackagePageProps {
+  params: {
+    packageName: string;
+  };
+}
+export default function Head({ params }: PackagePageProps) {
+  return (
+    <>
+      <title>{`RoundedNPM - ${params.packageName}`}</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+    </>
+  );
+}
