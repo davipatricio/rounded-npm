267c2e31eb40f586815d48cd920dce96f07ddb62
Davi Patricio <davipatricio2006@gmail.com>
2023-02-02 17:36:52 +0000

    disable mirrors

--- src/utils/queryUtils.ts
+++ src/utils/queryUtils.ts
@@ -1,7 +1,9 @@
 import { getPackument } from "query-registry";
 
 export async function getPackageInfo(name: string, version = "latest") {
-  const data = await getPackument({ name });
+  // Don't use mirrors for this query
+  const data = await getPackument({ name, mirrors: [] });
+
   // try to get a tag with value of version
   let versionInfo = data.versions[data["dist-tags"][version]];
   // if no tag found, try to get a version with value of version
